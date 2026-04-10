f2a4b4619412fc088f74a866ca1d9f4d8b42af65
piasmajumdar <piasmajumdar@gmail.com>
2026-04-10 22:55:30 +0530

    form created and onSubmit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SimpleForm from './Components/SimpleForm/SimpleForm'
+
+function App() {
+
+  return (
+    <>
+      <h1>Explore React form</h1>
+      <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
